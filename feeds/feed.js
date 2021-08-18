68238fbfebfe1f16159d08a3e061f8467a0dc8ef
shhh7612 <yogscast.sebastian@gmail.com>
2021-08-17 20:03:31 -0500

    Update feed.js

--- feeds/feed.js
+++ feeds/feed.js
@@ -14,14 +14,15 @@ module.exports = {
                     Object.entries(body.query.recentchanges).forEach(([key, value]) => {
                         const found = data.fandom.find(element => element === `${value.revid}${value.pageid}${value.userid}`);
                         if(found === undefined){
-                            const post = value; let type = null, color = colors.main, comment = `(${post.parsedcomment})`;
+                            const post = value; let type = null, color = colors.main, comment = `(${post.parsedcomment})`, length = null;
+                            if(post.newlen - post.oldlen >= 0){length = `+${post.newlen - post.oldlen}`}else{`${post.newlen - post.oldlen}`}
                             if(post.type === 'log'){if(post.logtype === 'upload'){type = 'uploaded'} if(post.logtype === 'delete'){type = 'deleted'} if(post.logtype === 'move'){type = 'moved'}}
                             if(post.type === 'edit'){type = 'edited'}
                             if(post.type === 'new'){type = 'created page'}
                             if(post.minor !== undefined){comment = `*Minor Edit* (${post.parsedcomment})`}
                             if(post.parsedcomment === ''){comment = ''}
                             if(type === null){console.log(value); type = post.type; color = colors.red}
-                            channel.send({embeds:[new MessageEmbed().setDescription(`[${post.user}](${wiki}User:${(post.user).replace(/ /g,"_")}) ${type} [${post.title}](${wiki}${(post.title).replace(/ /g,"_")}) ( [cur](${wiki}${(post.title).replace(/ /g,"_")}?curid=${post.pageid}&diff=0&oldid=${post.revid}) | [prev](${wiki}${(post.title).replace(/ /g,"_")}?curid=${post.pageid}&diff=${post.revid}&oldid=${post.old_revid}) ) (${post.newlen - post.oldlen}) ${comment}`).setColor(color)]})
+                            channel.send({embeds:[new MessageEmbed().setDescription(`[${post.user}](${wiki}User:${(post.user).replace(/ /g,"_")}) ${type} [${post.title}](${wiki}${(post.title).replace(/ /g,"_")}) ([cur](${wiki}${(post.title).replace(/ /g,"_")}?curid=${post.pageid}&diff=0&oldid=${post.revid}) | [prev](${wiki}${(post.title).replace(/ /g,"_")}?curid=${post.pageid}&diff=${post.revid}&oldid=${post.old_revid})) (${length}) ${comment}`).setColor(color).setTimestamp()]})
                             data.fandom.push(`${post.revid}${post.pageid}${post.userid}`)
                             writeData(data);
                         }
@@ -34,18 +35,20 @@ module.exports = {
             client.channels.fetch(reddit.channel).then(channel => {
                 const Parser = require('rss-parser'), parser = new Parser({maxRedirects: 2}), post = [];
                 (async () => {
-                    let feed = await parser.parseURL(reddit.link);
-                    feed.items.forEach(async item => {
-                        const found = data.reddit.find(element => element === item.id);
-                        if(found === undefined){
-                            fetch(`${item.link}.json?limit=5`).then(async response => {
-                                try {let body = await response.json();post.push(body[0].data.children[0].data);}catch(error){}
-                            }).catch(error => {}).then(() => {
-                                channel.send({embeds:[new MessageEmbed().setTitle(`Reddit: ${item.title}`).setURL(item.link).setDescription(post[0].selftext).setImage(post[0].url).setColor(colors.main)]})
-                                data.reddit.push(item.id); writeData(data);
-                            })
-                        }
-                    });
+                    try{
+                        let feed = await parser.parseURL(reddit.link);
+                        feed.items.forEach(async item => {
+                            const found = data.reddit.find(element => element === item.id);
+                            if(found === undefined){
+                                fetch(`${item.link}.json?limit=5`).then(async response => {
+                                    try {let body = await response.json();post.push(body[0].data.children[0].data);}catch(err){console.log(err)}
+                                }).catch(error => {}).then(() => {
+                                    channel.send({embeds:[new MessageEmbed().setTitle(`Reddit: ${item.title}`).setURL(item.link).setDescription(post[0].selftext).setImage(post[0].url).setColor(colors.main).setTimestamp()]})
+                                    data.reddit.push(item.id); writeData(data);
+                                })
+                            }
+                        });
+                    }catch(err){}
                 })();
             }).catch(console.error);
         }, (reddit.cooldown * 1000))
@@ -54,10 +57,12 @@ module.exports = {
         setInterval(()=>{
             client.channels.fetch(twitter.channel).then(channel => {
                 T.get('statuses/user_timeline', {screen_name:'amane_kakuyomu', exclude_replies:true, include_rts:false, count:5}, function(err, tweets, response){
-                    tweets.forEach(async tweet => {
-                        const found = data.twitter.find(element => element === tweet.id_str);
-                        if(found === undefined){channel.send(`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`); data.twitter.push(tweet.id_str); writeData(data);}
-                    });
+                    try {
+                        tweets.forEach(async tweet => {
+                            const found = data.twitter.find(element => element === tweet.id_str);
+                            if(found === undefined){channel.send(`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`); data.twitter.push(tweet.id_str); writeData(data);}
+                        });
+                    } catch(error){}
                 })
             })
         }, (twitter.cooldown * 1000))
