47c68ded1404843663e2babb5fcf0cee3e43c1c7
shhh7612 <yogscast.sebastian@gmail.com>
2021-08-28 21:28:16 -0500

    Update feed.js

--- feeds/feed.js
+++ feeds/feed.js
@@ -2,7 +2,7 @@ const {colors, feedsData, feedsJson} = require('../config.json')
 const data = require(feedsData)
 const {MessageEmbed} = require('discord.js')
 const fetch = require('node-fetch'); 
-const {fandom, twitter, reddit} = require(feedsJson);
+const {fandom, twitter, reddit, redditComments} = require(feedsJson);
 module.exports = {
     start(client){
         //Ryoushin no Shakkin Wiki: Recent changes
@@ -22,7 +22,7 @@ module.exports = {
                             if(post.minor !== undefined){comment = `*Minor Edit* (${post.parsedcomment})`}
                             if(post.parsedcomment === ''){comment = ''}
                             if(type === null){console.log(value); type = post.type; color = colors.red}
-                            channel.send({embeds:[new MessageEmbed().setDescription(`[${post.user}](${wiki}User:${(post.user).replace(/ /g,"_")}) ${type} [${post.title}](${wiki}${(post.title).replace(/ /g,"_")}) ([cur](${wiki}${(post.title).replace(/ /g,"_")}?curid=${post.pageid}&diff=0&oldid=${post.revid}) | [prev](${wiki}${(post.title).replace(/ /g,"_")}?curid=${post.pageid}&diff=${post.revid}&oldid=${post.old_revid})) (${length}) ${comment}`).setColor(color).setTimestamp()]})
+                            channel.send({embeds:[new MessageEmbed().setDescription(`[${post.user}](${wiki}User:${(post.user).replace(/ /g,"_")}) ${type} [${post.title}](${wiki}${(post.title).replace(/ /g,"_")}) ([cur](${wiki}${(post.title).replace(/ /g,"_")}?curid=${post.pageid}&diff=0&oldid=${post.revid}) | [diff](${wiki}${(post.title).replace(/ /g,"_")}?curid=${post.pageid}&diff=${post.revid}&oldid=${post.old_revid})) (${length}) ${comment}`).setColor(color).setTimestamp(new Date(post.timestamp))]})
                             data.fandom.push(`${post.revid}${post.pageid}${post.userid}`)
                             writeData(data);
                         }
@@ -49,7 +49,7 @@ module.exports = {
                                         let parts = (post.media_metadata[Object.keys(post.media_metadata)[Object.keys(post.media_metadata).length - 1]].s.u).split('amp;');
                                         image = parts.join('');
                                     }
-                                    channel.send({embeds:[new MessageEmbed().setTitle(item.title).setURL(item.link).setDescription(stp.join('')).setImage(image).setColor(colors.main).setTimestamp()]})
+                                    channel.send({embeds:[new MessageEmbed().setTitle(item.title).setURL(item.link).setDescription(stp.join('')).setImage(image).setColor(colors.main).setFooter(`Posted by: ${post.author}`).setTimestamp(new Date((post.created * 1000)))]})
                                     data.reddit.push(item.id); 
                                     writeData(data);
                                 }).catch(error => {})
@@ -59,40 +59,74 @@ module.exports = {
                 })();
             }).catch(console.error);
         }, (reddit.cooldown * 1000))
+        //Ryoushin no Shakkin Reddit: Comments
+        setInterval(()=>{
+            client.channels.fetch(redditComments.channel).then(channel => {
+                try{
+                    fetch(redditComments.link).then(response => response.json()).then(comments =>{
+                        comments.data.children.forEach(comment => {
+                            const found = data.reddit.find(element => element === comment.data.name);
+                            if(found === undefined){
+                                channel.send({
+                                    embeds:[
+                                        new MessageEmbed()
+                                        .setTitle(`${comment.data.link_title}`)
+                                        .setURL(`${comment.data.link_permalink}/${comment.data.id}/`)
+                                        .setDescription(comment.data.body)
+                                        .setColor(colors.main)
+                                        .setFooter(`Commented by: ${comment.data.author}`)
+                                        .setTimestamp(new Date((comment.data.created * 1000)))
+                                    ]
+                                })
+                                data.reddit.push(comment.data.name); 
+                                writeData(data);
+                            }
+                        })
+                    })
+                }catch(err){}
+            }).catch(console.error);
+        }, (redditComments.cooldown * 1000))
         //Twitter: @amane_kakuyomu New Posts
         const Twit = require('twit'), T = new Twit({consumer_key: process.env.tck,consumer_secret: process.env.tcs,access_token: process.env.tat,access_token_secret: process.env.tats})
         setInterval(()=>{
             client.channels.fetch(twitter.channel).then(channel => {
                 T.get('statuses/user_timeline', {screen_name:'amane_kakuyomu', exclude_replies:true, include_rts:false, count:5, tweet_mode:'extended'}, function(err, tweets, response){
-                    tweets.forEach(tweet => {
-                        const found = data.twitter.find(element => element === tweet.id_str);
-                        if(found === undefined){
-                            let media = null, extra = [], text = null;
-                            if(/(https:\/\/t.co\/)\w+/g.test(tweet.full_text)){text = tweet.full_text.replace(/(https:\/\/t.co\/)\w+/gi, '')}else{text = tweet.full_text}
-                            if(tweet.extended_entities !== undefined){media = tweet.extended_entities.media[0].media_url_https}
-                            tweet.entities.urls.forEach(u => {
-                                if(/(https:\/\/twitter.com\/)\w+/g.test(u.expanded_url)){
-                                    let id = u.expanded_url.replace(/.+(?=status)status\//, '');
-                                    T.get('statuses/show/:id', { id: id }, function(err, data, res){
-                                        let dqs = data.quoted_status;
-                                        extra.push(`\n\n**Tweet embed:** [${dqs.user.name} (@${dqs.user.screen_name})](${u.expanded_url})\n${dqs.text}`)
-                                        if(dqs.entities.media !== undefined){media = dqs.entities.media.media_url}
-                                    })
-                                }else{extra.push(`\n**${u.expanded_url}**`)}
-                            })
-                            channel.send({embeds:[
-                                new MessageEmbed()
-                                .setAuthor(`${tweet.user.name} (@${tweet.user.screen_name})`, tweet.user.profile_image_url, `https://twitter.com/${tweet.user.screen_name}`)
-                                .setDescription(`${text} ${extra}\n\n[Click to view full tweet on Twitter](https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str})`)
-                                .setImage(media)
-                                .setColor(colors.main)
-                                .setFooter('Twitter', 'https://abs.twimg.com/icons/apple-touch-icon-192x192.png')
-                                .setTimestamp(new Date(tweet.created_at))
-                            ]})
-                            data.twitter.push(tweet.id_str); 
-                            writeData(data);
-                        }
-                    });
+                    try{
+                        tweets.forEach(tweet => {
+                            const found = data.twitter.find(element => element === tweet.id_str);
+                            if(found === undefined){
+                                let media = null, extra = [], text = null;
+                                if(/(https:\/\/t.co\/)\w+/g.test(tweet.full_text)){text = tweet.full_text.replace(/(https:\/\/t.co\/)\w+/gi, '')}else{text = tweet.full_text}
+                                if(tweet.extended_entities !== undefined){media = tweet.extended_entities.media[0].media_url_https}
+                                tweet.entities.hashtags.forEach(tag=>{
+                                    text = text.replace(`#${tag.text}`, `[#${tag.text}](https://twitter.com/hashtag/${tag.text})`)
+                                })
+                                tweet.entities.urls.forEach(u => {
+                                    if(/(https:\/\/twitter.com\/)\w+/g.test(u.expanded_url)){
+                                        let id = u.expanded_url.replace(/.+(?=status)status\//, '');
+                                        T.get('statuses/show/:id', { id: id }, function(err, data, res){
+                                            try{
+                                                let dqs = data.quoted_status;
+                                                extra.push(`\n\n**Tweet embed:** [${dqs.user.name} (@${dqs.user.screen_name})](${u.expanded_url})\n${dqs.text}`)
+                                                if(dqs.entities.media !== undefined){media = dqs.entities.media.media_url}
+                                            }catch(e){console.log(e); console.log("tweet", tweet, "data", data)}
+                                        })
+                                    }else{extra.push(`\n${u.expanded_url}`)}
+                                })
+                                channel.send({embeds:[
+                                    new MessageEmbed()
+                                    .setAuthor(`${tweet.user.name} (@${tweet.user.screen_name})`, tweet.user.profile_image_url, `https://twitter.com/${tweet.user.screen_name}`)
+                                    .setDescription(`${text} ${extra}\n\n[**Click to view full tweet on Twitter**](https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str})`)
+                                    .setImage(media)
+                                    .setColor(colors.main)
+                                    .setFooter('Twitter', 'https://abs.twimg.com/icons/apple-touch-icon-192x192.png')
+                                    .setTimestamp(new Date(tweet.created_at))
+                                ]})
+                                data.twitter.push(tweet.id_str); 
+                                writeData(data);
+                            }
+                        });
+                    }catch(e){console.log(e)}
                 })
             })
         }, (twitter.cooldown * 1000))
